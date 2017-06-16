a8d25e6a4f543162593ad5bd16f31d7399dd1fcb
kboygit <heybisayangdako@gmail.com>
2017-06-16 22:37:44 +0000

    completed image_blur#1 modified array.rb

--- convert-intro/array.rb
+++ convert-intro/array.rb
@@ -1,4 +1,6 @@
 fruits = []
+index = [1, 2, 3, 4, 5, 6, 7, 8]
+
 fruits << "Apples"
 fruits << "Oranges"
 fruits << "Strawberries"
@@ -7,4 +9,12 @@ fruits << "Mangos"
 fruits.each do |fruits|
   x = fruits
   puts "Om nom nom #{x}"
+end
+
+index.drop(2).each do |index|
+    puts "index's value is #{index}"
+end
+
+index.drop(0).each do |index|
+    puts "index's value is #{index}"
 end
--- image_blur#1.rb
+++ image_blur#1.rb
@@ -5,10 +5,12 @@ class Image
       @image = image
     end
 
+# Comment out method output_image then used method output to get an output from line comment no. 32 - 35
     def output_image
       puts "#{self.image}"
     end
 
+# remove .output_image then change to put arr.join to get an output from line comment no. 32 - 35
     def output
       @image.each do |arr|
         arr.join.output_image
@@ -20,10 +22,15 @@ end
 # image = Array.new(4, 0) { Array.new(4, 0)
 # Inserts a new element to an array at any position
 # image.insert(1, 1)
-image = Array.new([
+image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
   [0, 0, 0, 1],
-  [0, 0, 0, 0],
+  [0, 0, 0, 0]
   ])
+# change to image_output to get an output of:
+# 0000
+# 0100
+# 0001
+# 0000
 image.output_image
